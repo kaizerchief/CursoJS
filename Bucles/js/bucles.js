ae3bfced764e3cee477b4a09f9f104d56a4160d0
Miguelangel Torres <miguel.torresdev@gmail.com>
2020-05-11 07:28:18 -0400

    Terminado el bloque de bucles

---
+++ Bucles/js/bucles.js
@@ -0,0 +1,24 @@
+'use strict'
+/*
+//BUCLE FOR
+ var numero = 100;
+
+for(var i = 0 ; i <= numero ; i++){
+    console.log("Vamos por el numero "+i);
+}
+
+//BUCLE WHILE
+var year = 2018;
+
+while(year <= 2051){
+    console.log("Estamos en el año "+year);
+    year++;//OPERADOR DE INCREMENTO NECESARIO PARA EVITAR SE VUELVA INFINITO EL BUCLE
+}
+*/
+//BUCLE DO WHILE
+var years = 10;
+
+do{
+    alert("Solo cuando sea diferente a 20 y es "+years);
+    years = 20;
+}while(years != 20)
