eafb30bbca2cbbfe3f6d8f47f1b3f3df4ed14d8c
Miguelangel Torres <miguel.torresdev@gmail.com>
2020-05-11 07:38:16 -0400

    Terminado el bloque de alertas.

---
+++ Alertas/js/alertas.js
@@ -0,0 +1,15 @@
+'use strict'
+/*
+//ALERTA
+alert("Hola!");
+
+//CONFIRMACION
+var resultado;
+resultado = confirm("¿Esta seguro de querer continuar?");
+console.log("Su respuesta fue "+resultado);
+*/
+//INGRESO DE DATOS
+//TODOS LOS DATOS INGRESADOS SON DE TIPO STRING
+var ingreso;
+ingreso = prompt("¿Que edad tienes?", 18);
+console.log("Ingresaste la edad de "+ingreso +" años");
