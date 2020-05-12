8591c093152544e74ef8eb08ca2575f01be84a9f
Miguelangel Torres <miguel.torresdev@gmail.com>
2020-05-12 07:38:58 -0400

    Terminado el ejercicio 2

--- Ejercicios_01/js/ejercicio1.js
+++ Ejercicios_01/js/ejercicio1.js
@@ -1,14 +1,18 @@
 'use strict'
-
+//EJERCICIO 1
 //PROGRAMA QUE PIDA DOS NUMEROS Y NOS DIGA CUAL ES MAYOR, EL MENOR Y SI SON IGUALES
+//ADICIONALMENTE HACER QUE SOLO PERMITA EL INGRESO DE NUMEROS 
+//SOLO NUMEROS Y MAYORES A 0
 var numero1;
 var numero2;
 var mensaje = "";
 
-numero1 = parseInt(prompt("Introduce el primer numero", 0));
-numero2 = parseInt(prompt("Introduce el segundo numero", 0));
+while(numero1 <= 0 || numero2 <= 0 || isNaN(numero1) || isNaN(numero2)){ //LA FUNCION isNaN DEVUELVE UN VALOR TRUE SI EL VALOR NO ES NUMERICO, POR LO CUAL SE PIDE DE NUEVO.
+    numero1 = parseInt(prompt("Introduce el primer numero", 0));
+    numero2 = parseInt(prompt("Introduce el segundo numero", 0));
+}
 
-console.log("Ingresaste los siguientes numeros "+numero1+" "+numero2+" Ahora vamosa compararlos");
+console.log("Ingresaste los siguientes numeros "+numero1+" "+numero2+" Ahora vamos a compararlos");
 
 //COMENZAMOS A COMPARAR
 if(numero1 == numero2){
---
+++ Ejercicios_01/js/ejercicio2.js
@@ -0,0 +1,34 @@
+'use strict'
+
+//UTILZANDO UN BUCLE, CALCULAR SUMA Y LA MEDIA DE LOS NUMEROS INTRODUCIDOS
+//HASTA INTRODUCIR UN NUMERO NEGATIVO Y AHI MOSTRAR EL RESULTADO
+var numero;
+var esNumero = false;
+var suma = 0;
+var media;
+var cantidad = 0;
+
+//BUCLE QUE PIDE LOS NUMEROS
+do{
+    while(esNumero == false){//USAMOS UN BUCLE WHILE PARA SOLO PERMITIR EL INGRESO DE NUMEROS Y NO CARACTERES O SIMBOLOS.
+        numero = parseInt(prompt("Ingresa un numero Cantidad: "+cantidad, 0));
+        if(isNaN(numero) == false){
+            esNumero = true;
+        }
+    }
+    if(numero >= 0){//FILTRO PARA NO CONTAR EL NUMERO NEGATIVO EN EL CALCULO FINAL.
+        suma += numero;
+        cantidad++;
+    }
+    esNumero = false;//RETORNAMOS EL VALOR A FALSE PARA PODER REPETIR EL CICLO
+}while(numero >= 0)
+
+//CALCULAMOS LA MEDIA
+media = suma / cantidad;
+
+//MOSTRAMOS EL RESULTADO FINAL
+console.log("La suma de los numeros ingresados es: "+suma);
+console.log("La media de los numero es: "+media);
+console.log("La cantidad de numeros ingresados fue: "+cantidad);
+console.log("El numero negativo ingresado fue: "+numero);
+
