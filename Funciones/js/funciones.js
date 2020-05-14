ca8905c00f15e5b551c886a4061d3090d341e90c
Miguelangel Torres <miguel.torresdev@gmail.com>
2020-05-14 07:39:56 -0400

    Terminada la clase de funciones.

---
+++ Funciones/js/funciones.js
@@ -0,0 +1,14 @@
+'use strict'
+//FUNCIONES
+function calculadora(){
+    //INSTRUCCIONES A EJECUTAR
+    console.log("Si soy yo");
+    //RETURN DEVUELVE UN VALOR EN LA FUNCION.
+    return "Hola soy la calculadora.";
+}
+
+//INVOCANDO EL RETURN DE LA FUNCION
+console.log(calculadora());
+
+//INVOCAR O LLAMAR LA FUNCION
+calculadora();
