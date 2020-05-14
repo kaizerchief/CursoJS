7847e71263cd96d50f985664dd49bd786fd354a8
Miguelangel Torres <miguel.torresdev@gmail.com>
2020-05-14 08:01:30 -0400

    Terminada la clase de parametros

---
+++ Funciones/js/parametros.js
@@ -0,0 +1,32 @@
+'use strict'
+//PARAMETROS EN FUNCIONES
+var n1, n2;
+n1 = 3;
+n2 = 3;
+/*
+function calculadora(n1, n2){//LOS PARAMETROS SE INSERTAN EN LOS PARENTESIS DE LA FUNCION
+    var suma = n1 + n2;
+    var resta = n1 - n2;
+    var multi = n1 * n2;
+    var div = n1 / n2;
+
+    return suma;
+}
+
+console.log(calculadora(n1, n2)); 
+
+//PARAMETROS OPCIONALES
+function calculadora(n1, n2, mostrar = false) {
+    if(mostrar == false){
+        console.log(n1+n2);
+        console.log("me imprimo");
+    }else{
+        console.log(n1*n2);
+        console.log("ya no me imprimo.")
+    }
+  }
+
+  calculadora(n1, n2);
+  calculadora(2, 2, true);
+*/
+//FUNCIONES DENTRO DE OTRAS
