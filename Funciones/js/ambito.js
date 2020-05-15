d80ef75d7c88b4b935f0a937f9bb77a3da03f992
Miguelangel Torres <miguel.torresdev@gmail.com>
2020-05-15 07:03:58 -0400

    Terminada la clase del Ambito de Variables

---
+++ Funciones/js/ambito.js
@@ -0,0 +1,12 @@
+'use strict'
+//AMBITO DE LAS VARIABLES
+
+function holaMundo(texto){
+    var hola_mundo = "Texto dentro de funcion"; //VARIABLE LOCAL
+    console.log(texto);
+    console.log(numero.toString());//ACCEDIENDO A UNA VARIABLE GLOBAL DESDE DENTRO DE UNA FUNCION Y EL METODO TOSTRING CONVIERTE EL VALOR A STRING
+}
+
+var nuumero = 12;
+var texto = "Hola mundo, soy una variable global"; //VARIABLE GLOBAL
+holaMundo(texto);
