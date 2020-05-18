24d355a4803a2c61100c87cb3556e7075e7e41fc
Miguelangel Torres <miguel.torresdev@gmail.com>
2020-05-18 07:32:37 -0400

    Terminada la clase de arrays clasicos

---
+++ Arreglos/js/arreglos_basicos.js
@@ -0,0 +1,9 @@
+'use strict'
+//ARRAYS, ARREGLOS, MATRICES
+var nombres = ["Juan", "Pedro", "Jose"];
+
+console.log(nombres);//IMPRESION DE TODO EL ARRAY
+console.log(nombres[2]);//IMPRESION DE ELEMENTO ESPECIFICO POR SU POSICION
+
+//PROPIEDADES DE ARRAYS
+
